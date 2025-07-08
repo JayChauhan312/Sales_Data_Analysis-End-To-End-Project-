c5a0cf1de717f2a14853f3191af9df9e579b9ddb
JayChauhan312 <jaichauhan0312@gmail.com>
2025-07-08 15:50:05 +0530

    Update 27_June_Project_SalesAnalysis.py

--- 27_June_Project_SalesAnalysis.py
+++ 27_June_Project_SalesAnalysis.py
@@ -1,6 +1,6 @@
 ### *Problem Statement*
 
-''' XYZ Co's 2014-2018 sales data to identify key revenue and profit drivers across products, channels
+''' MI Co's 2014-2018 sales data to identify key revenue and profit drivers across products, channels
  and regions, uncover seasonal trends and outliers, and align performance against budgets.
  Use these insights to optimize pricing, promotions and market expansion for sustainable growth
  and reduced concentration risk. I
